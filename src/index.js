b903667d63b4fe4f3ed7caf28cb82bcb3685080f
mondirhallouli <halloulim@gmail.com>
2022-06-09 14:40:18 +0100

    added a new route to the product page component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Products from "./components/Products";
 import Cart from "./components/Cart";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import Product from "./components/Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,6 +17,7 @@ root.render(
         <Route path="/" element={<App />}>
           <Route index element={<Products />} />
           <Route path="cart" element={<Cart />} />
+          <Route path="product/:productId" element={<Product />} />
         </Route>
       </Routes>
     </BrowserRouter>
