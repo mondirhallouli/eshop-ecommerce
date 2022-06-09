2c6973f6eca1c9661be9f836c7dea2d9b2c1abf3
mondirhallouli <halloulim@gmail.com>
2022-06-09 14:39:38 +0100

    changed the font of the price, limited the decimal to two digits

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./ProductCard.css";
 import { addToCart } from "../features/cart/cartSlice";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 function ProductCard({ id, title, price, category, img }) {
   const dispatch = useDispatch();
@@ -18,16 +19,16 @@ function ProductCard({ id, title, price, category, img }) {
     );
   };
   return (
-    <div className="product-card">
+    <Link to={`/product/${id}`} className="product-card">
       <div className="product-card_img">
         <img src={img} alt={title} />
       </div>
       <h2 className="product-card_title">{title.substring(0, 24)}...</h2>
-      <h3 className="product-card_price">${price}</h3>
+      <h3 className="product-card_price">${price.toFixed(2)}</h3>
       <button className="product-card_add-to-cart" onClick={handleClick}>
         ADD TO CART
       </button>
-    </div>
+    </Link>
   );
 }
 
