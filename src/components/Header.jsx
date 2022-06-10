f70a389a6b5e6fa13597423d00449290d89d1873
mondirhallouli <halloulim@gmail.com>
2022-06-10 15:47:08 +0100

    added a logo, adjusted the margins

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import logo from "../assets/logo.svg";
 
 function Header() {
   const cartCount = useSelector((state) => state.cart.cartItems.length);
   return (
     <div className="header">
       <Link to="/" className="header_brand">
-        E-Shop
+        <img src={logo} alt="logo" />
       </Link>
       <Link to="/cart" className="header_cart">
         Cart <span className="header_cart-count">({cartCount})</span>
